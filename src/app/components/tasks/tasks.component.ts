925b11ee9700b990577563e9047c7eb038ac4ddb
Joacop2002 <jpranteda99@gmail.com>
2023-01-18 23:07:08 -0300

    Finalizamos lista de tareas

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { TaskService } from '../../service/task.service';
 import { Task } from '../../Task';
 
@@ -23,5 +23,23 @@ export class TasksComponent implements OnInit {
    });
   }
 
+  deleteTask(task:Task){
+    this.taskService.deleteTask(task).subscribe(()=>{
+    this.tasks = this.tasks.filter( (t) => { return t.id !== task.id})
+    })
+    }
 
-}
+    toggleReminder(task: Task){
+      task.reminder = !task.reminder
+      this.taskService.updateTaskReminder(task).subscribe();
+    }
+
+    addTask(task:Task){
+     this.taskService.addTask(task).subscribe((task)=>{
+      this.tasks.push(task)
+     })
+    }
+  }
+
+
+
